ec3f082f908f58011ab5e2d944a5673f23bd94df
HimalBasnet18 <himalbasnet18@gmail.com>
2024-01-18 21:31:04 -0600

    Controller Updated

--- src/main/java/com/takeo/rest/PostController.java
+++ src/main/java/com/takeo/rest/PostController.java
@@ -1,8 +1,11 @@
 package com.takeo.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,5 +44,17 @@ public class PostController {
 		}
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found, post not created");
+	}
+	@GetMapping("/posts/{uid}")
+	public ResponseEntity<List<Post>> getAll(@PathVariable long uid)
+	{
+	List<Post> posts=postServiceImpl.read();
+	if(posts!=null)
+	{
+		return ResponseEntity.ok(posts);
+	}
+	else
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No posts avialable for this User");
+
 	}
 }
